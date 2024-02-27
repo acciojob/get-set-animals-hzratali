629f0bb0efaf3747e7b7632f5c1c3571c0670e61
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-02-27 16:30:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //complete this code
-class Animal {}
-
-class Dog extends Animal {}
-
-class Cat extends Animal {}
-
+class Animal {
+	constructor(private _species: string) {}
+	get species(): string {
+		return this._species;
+	}
+	makeSound(): void {
+		console.log(`The ${this._species} makes a sound`);
+	}
+}
+class Dog extends Animal {
+	bark(): void {
+		console.log("woof");
+	}
+}
+class Cat extends Animal {
+	purr(): void {
+		console.log("purr");
+	}
+}
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
